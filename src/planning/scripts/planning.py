71a596ec7f9d31baf918469a5b7c6a901d688a47
Morin Sol_Han <dream4future@kaist.ac.kr>
2021-06-09 17:03:14 +0900

    map base planning

--- src/planning/scripts/planning.py
+++ src/planning/scripts/planning.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import rospy
+import cv2 as cv
 import numpy as np
 from d_star_lite import DStarLite
 from grid import OccupancyGridMap
@@ -25,17 +26,17 @@ TRAJ = 2
 OBSTACLE = 255
 
 # Global variable
-g_map = np.zeros((128, 128), dtype=np.uint8) # 512, 512
+g_map = np.zeros((512, 512), dtype=np.uint8) # 512, 512
 
 def map_cb(msgs):
     global g_map
     h = msgs.info.height
     w = msgs.info.width
     input = np.transpose(np.reshape(msgs.data, (h,w)))
-    batch = 16 # 4
+    batch = 4 # 4
     for i in range(h):
         for j in range(w):
-            if input[i, j] == 100:
+            if input[i, j] > 99:
                 input[i, j] = 255
             else:
                 input[i, j] = 0
@@ -46,20 +47,21 @@ class Planner():
     def __init__(self, cnt, init_pose, goal):
         super(Planner, self).__init__()
         self.init_pose = tuple(init_pose.astype(int))
-        self.curr_pose = tuple(np.zeros((2)).astype(int))
+        self.curr_pose = tuple(init_pose.astype(int))
         self.goal = tuple(goal.astype(int))
         self.curr_ori = Quaternion()
         self.map = OccupancyGridMap(512, 512)
+        self.path = OccupancyGridMap(512, 512)
         self.ctrl = Twist()
         self.cnt = cnt
+        self.k = 0
 
-        self.pose_sub = rospy.Subscriber("/robot"+str(cnt)+"/slam_out_pose", PoseStamped, queue_size=10)
+        self.pose_sub = rospy.Subscriber("/robot"+str(cnt)+"/slam_out_pose", PoseStamped, self.pose_cb, queue_size=10)
         self.ctrl_pub = rospy.Publisher("/robot"+str(cnt)+"/cmd_vel", Twist, queue_size=10)
 
     def pose_cb(self, msgs):
-        self.curr_pose = np.array([msgs.pose.position.x, msgs.pose.position.y]).astype(int)
+        self.curr_pose = tuple((256*np.ones((2)) + 4*np.array([msgs.pose.position.x, msgs.pose.position.y])).astype(int))
         self.curr_ori = msgs.pose.orientation
-        print('pose'+str(self.cnt))
 
     def obtain_map(self):
         global g_map
@@ -82,11 +84,18 @@ class Planner():
         # # move and compute path
         path, g, rhs = dstar.move_and_replan(robot_position=self.curr_pose)
 
+        print(path)
+        for (x, y) in path:
+            if not g_map[x, y]:
+                g_map[x, y] = 255
+        cv.imshow("map" + str(self.cnt), np.array(g_map, dtype=np.uint8))
+        key = cv.waitKey(3000)
+
         self.control()
 
     def control(self):
         # Calculate control input to publish
-        self.ctrl.linear.x = 0.5
+        self.ctrl.linear.x = 0.0
         self.ctrl.linear.y = 0.0
         self.ctrl.linear.z = 0.0
 
@@ -99,7 +108,7 @@ class Planner():
 
     def main(self):
         try:
-            rate = rospy.Rate(1)
+            rate = rospy.Rate(0.5)
             while not rospy.is_shutdown():
                 self.planning()
                 rate.sleep()
@@ -112,8 +121,8 @@ if __name__ == '__main__':
     try:
         rospy.init_node('planning', anonymous=False)
         map_sub = rospy.Subscriber("/map_merge/map", OccupancyGrid, map_cb, queue_size=10)
-        init_pose = 64*np.ones((6)) + np.array([-2, -1, -2, 4, 8, 2]) # 256 / -7, -5, -7, 16, 24, 6
-        goal = 64*np.ones((2)) + np.array([5, -1]) # 256 / 20, -2
+        init_pose = 256*np.ones((6)) + np.array([-30, -22, -30, 62, 95, 21]) # 256 / -7, -5, -7, 16, 24, 6   -2, -1, -2, 4, 8, 2
+        goal = 256*np.ones((2)) + np.array([80, -8]) # 256 / 20, -2   5, -1
         cnt = rospy.get_param('~cnt')
         planner = Planner(cnt, init_pose[2*cnt-2:2*cnt], goal)
         planner.main()
